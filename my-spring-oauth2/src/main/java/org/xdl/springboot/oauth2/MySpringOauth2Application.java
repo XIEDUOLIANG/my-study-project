b4156c749ca3abab434bae7f0215045d6de039ba
xieduoliang <govbhis@163.com>
2021-10-20 10:21:48 +0800

    oauth2 add

---
+++ my-spring-oauth2/src/main/java/org/xdl/springboot/oauth2/MySpringOauth2Application.java
@@ -0,0 +1,16 @@
+package org.xdl.springboot.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XieDuoLiang
+ * @date 2021/10/20 上午9:50
+ */
+@SpringBootApplication
+public class MySpringOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringOauth2Application.class,args);
+    }
+}

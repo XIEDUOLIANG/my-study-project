6da431fba90b5eaab59f94baa211803f10acf73d
xieduoliang <949957765@qq.com>
2021-12-24 17:20:19 +0800

    init rabbitmq project

--- my-rabbitmq/my-rabbitmq-provider/src/main/java/org/xdl/rabbitmq/provider/config/DirectExchangeConfig.java
+++ my-rabbitmq/my-rabbitmq-provider/src/main/java/org/xdl/rabbitmq/provider/config/DirectExchangeConfig.java
@@ -1,14 +1,36 @@
 package org.xdl.rabbitmq.provider.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DirectExchangeConfig {
 
+    public static final String defaultQueueKey = "key_one";
+
+    @Autowired
+    private Queue queue;
+
+    @Autowired
+    private DirectExchange directExchange;
+
     @Bean
     public Queue queue() {
         return new Queue("defaultQueue",true);
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("defaultDirectExchange",true,false);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue).to(directExchange).with(defaultQueueKey);
     }
 }
